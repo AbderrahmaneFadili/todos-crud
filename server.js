f344e2e7b0eeb93700594a7f036b32b5b999201b
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-06-28 12:59:42 +0100

    Set up MongoDB Connection

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = require("./routes/routes");
+const mongoos = require("mongoose");
+const dotenv = require("dotenv");
 
 const PORT = 7000;
 
 const app = express();
 
 app.use(router);
+
+//config .env envirement
+dotenv.config();
+
+mongoos
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log(error));
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
